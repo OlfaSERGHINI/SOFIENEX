e406e748a680cb8ec9a9c4eea018a15894e3d858
OlfaSERGHINI <46780067+OlfaSERGHINI@users.noreply.github.com>
2019-02-01 21:31:47 +0100

    Update Application.java

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Application {
 
-	@RequestMapping("/")
+	@RequestMapping("/appdocker")
 	public String home() {
 		return "Hello Docker World";
 	}
